8de9fd564487b0897b7056772f13bc0784b8d92b
wpem3123 <wpem3123@naver.com>
2022-08-29 11:47:04 +0900

    Http 매핑 공부 완료

---
+++ src/main/java/site/metacoding/white/FirstController.java
@@ -0,0 +1,37 @@
+package site.metacoding.white;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// Http Method -> Mapping 기술 
+// Get -> 주소창, 하이퍼링크
+// Post, GET  -> form 태그
+// PUT,DELETE,GET,POST -> JS
+
+// 포스트맨 -> 4가지 요청을 쉽게 테스트 해볼수 있다.
+@RestControllerAdvice // data를 응답
+public class FirstController {
+	
+	
+	@GetMapping("/first")
+	public String getData() {
+		return "<h1>data</h1>";
+	}
+	@PostMapping("/first")
+	public String postData() {
+		return "<h1>insert data</h1>";
+	}
+	
+	
+	@PutMapping("/first")
+	public String putData() {
+		return "<h1>update data</h1>";
+	}
+	@DeleteMapping("/first")
+	public String deleteData() {
+		return "<h1>delete data</h1>";
+	}
+}
